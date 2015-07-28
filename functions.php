3451f7f2e0f8fdaa530c404c6efb8a1e5e560c29
dtbaker <dtbaker@gmail.com>
2015-07-29 01:13:37 +1000

    hex2bin php5.4

--- functions.php
+++ functions.php
@@ -579,3 +579,16 @@ function envato_get_item_reviews($item_id){
 
     return $reviews;
 }
+
+
+if ( !function_exists( 'hex2bin' ) ) {
+    function hex2bin( $str ) {
+        $sbin = "";
+        $len = strlen( $str );
+        for ( $i = 0; $i < $len; $i += 2 ) {
+            $sbin .= pack( "H*", substr( $str, $i, 2 ) );
+        }
+
+        return $sbin;
+    }
+}
