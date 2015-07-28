0d4e94678bcbe25225f96e157c6821d632faf9ab
dtbaker <dtbaker@gmail.com>
2015-07-28 23:34:17 +1000

    config

--- config.php
+++ config.php
@@ -2,36 +2,41 @@
 
 
 // This is the item name we are looking for in the API statement result:
-$file_name = 'Ultimate Client Manager - CRM - Pro Edition';
+if(!defined('_ENVATO_ITEM_NAME'))define('_ENVATO_ITEM_NAME', 'Ultimate Client Manager - CRM - Pro Edition');
+// This is the item ID we are using in API calls
+if(!defined('_ENVATO_ITEM_ID'))define('_ENVATO_ITEM_ID', 2621629);
 // This is our personal token generated from build.envato.com
-$personal_token = 'pemOG07IWPuvb8GCSum9lwvCVpkgHwj9';
-
-
+if(!defined('_ENVATO_PERSONAL_TOKEN'))define('_ENVATO_PERSONAL_TOKEN',  'pemOG07IWPuvb8GCSum9lwvCVpkgHwj9');
 
 
 set_time_limit(0);
 ini_set('display_errors',true);
 ini_set('error_reporting',E_ALL);
 date_default_timezone_set('Australia/Melbourne');
-define('_ENVATO_LSG_TEMPLATE','template_bg.png');
-define('_ENVATO_LSG_TEMPLATE_MASK','template_mask.png');
+
+if(!defined('_ENVATO_LSG_TEMPLATE'))define('_ENVATO_LSG_TEMPLATE','template_bg.png');
+if(!defined('_ENVATO_LSG_TEMPLATE_MASK'))define('_ENVATO_LSG_TEMPLATE_MASK','template_mask.png');
 define('_ENVATO_LSG_FONT',__DIR__.'/HelveticaNeue-Regular.ttf');
-$statement_cache = 'cache_statement.json'; // change this name or block it with .htaccess if you don't want people to see your recent sales.
-$statement_cache_timeout = 120; // please don't change this to anything lower. be nice to servers!
-$viewer_cache = 'cache_viewers.json'; // change this name or block it with .htaccess if you don't want people to see info
-$viewer_cache_timeout = 600; // how long to class someone as a visitor after they leave the page? default 10 minutes
-$debug = isset($_REQUEST['debug']);
-if(!file_exists($statement_cache)){
-    touch($statement_cache);
+define('_ENVATO_STATEMENT_CACHE_FILE','cache_statement.json');
+define('_ENVATO_STATEMENT_CACHE_TIMEOUT',600);
+define('_ENVATO_ITEM_CACHE_FILE','cache_item_'._ENVATO_ITEM_ID.'.json');
+define('_ENVATO_ITEM_CACHE_TIMEOUT',600);
+define('_VIEWER_CACHE_FILE','cache_viewers.json');
+define('_VIEWER_CACHE_TIMEOUT',600); // how long to class someone as a visitor after they leave the page? default 10 minutes
+define('_GIF_CACHE_TIMEOUT',30); // how long to keep static gif images cached
+define('_DTBAKER_DEBUG_MODE',isset($_REQUEST['debug']));
+
+if(!file_exists(_ENVATO_STATEMENT_CACHE_FILE)){
+    touch(_ENVATO_STATEMENT_CACHE_FILE);
 }
-if(!file_exists($statement_cache)){
-    echo "Unable to create ".$statement_cache.". Please ensure correct permissions.";
+if(!file_exists(_ENVATO_STATEMENT_CACHE_FILE)){
+    echo "Unable to create "._ENVATO_STATEMENT_CACHE_FILE.". Please ensure correct permissions.";
     exit;
 }
-if(!file_exists($viewer_cache)){
-    touch($viewer_cache);
+if(!file_exists(_VIEWER_CACHE_FILE)){
+    touch(_VIEWER_CACHE_FILE);
 }
-if(!file_exists($viewer_cache)){
-    echo "Unable to create ".$viewer_cache.". Please ensure correct permissions.";
+if(!file_exists(_VIEWER_CACHE_FILE)){
+    echo "Unable to create "._VIEWER_CACHE_FILE.". Please ensure correct permissions.";
     exit;
 }
