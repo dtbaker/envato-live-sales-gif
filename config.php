f7641d30d1c8e102822cc7c091a1485ed9bdac5f
dtbaker <dtbaker@gmail.com>
2015-07-29 00:33:20 +1000

    item id name lookup

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
 
 
-// This is the item name we are looking for in the API statement result:
-if(!defined('_ENVATO_ITEM_NAME'))define('_ENVATO_ITEM_NAME', 'Ultimate Client Manager - CRM - Pro Edition');
 // This is the item ID we are using in API calls
 if(!defined('_ENVATO_ITEM_ID'))define('_ENVATO_ITEM_ID', 2621629);
+// or you can pass it in a url parameter like:  gif.php?item_id=12345
+// if(!defined('_ENVATO_ITEM_ID'))define('_ENVATO_ITEM_ID', isset($_GET['item_id']) ? (int)$_GET['item_id'] : 0);
+// Your envato username
+if(!defined('_ENVATO_USERNAME'))define('_ENVATO_USERNAME', 'dtbaker');
 // This is our personal token generated from build.envato.com
 if(!defined('_ENVATO_PERSONAL_TOKEN'))define('_ENVATO_PERSONAL_TOKEN',  'pemOG07IWPuvb8GCSum9lwvCVpkgHwj9');
 
@@ -18,25 +20,22 @@ if(!defined('_ENVATO_LSG_TEMPLATE'))define('_ENVATO_LSG_TEMPLATE','template_bg.p
 if(!defined('_ENVATO_LSG_TEMPLATE_MASK'))define('_ENVATO_LSG_TEMPLATE_MASK','template_mask.png');
 define('_ENVATO_LSG_FONT',__DIR__.'/HelveticaNeue-Regular.ttf');
 define('_ENVATO_STATEMENT_CACHE_FILE','cache_statement.json');
-define('_ENVATO_STATEMENT_CACHE_TIMEOUT',600);
+define('_ENVATO_STATEMENT_CACHE_TIMEOUT',600); // 10 minutues
+define('_ENVATO_API_ITEM_CACHE_FILE','cache_api_item_'._ENVATO_ITEM_ID.'.json');
+define('_ENVATO_API_ITEM_CACHE_TIMEOUT',3600); // 1 hour
 define('_ENVATO_ITEM_CACHE_FILE','cache_item_'._ENVATO_ITEM_ID.'.json');
-define('_ENVATO_ITEM_CACHE_TIMEOUT',600);
-define('_VIEWER_CACHE_FILE','cache_viewers.json');
+define('_ENVATO_ITEM_CACHE_TIMEOUT',3600); // 1 hour
+define('_VIEWER_CACHE_FILE','cache_viewers_'._ENVATO_ITEM_ID.'.json');
 define('_VIEWER_CACHE_TIMEOUT',600); // how long to class someone as a visitor after they leave the page? default 10 minutes
 define('_GIF_CACHE_TIMEOUT',30); // how long to keep static gif images cached
-define('_DTBAKER_DEBUG_MODE',isset($_REQUEST['debug']));
+define('_DTBAKER_DEBUG_MODE',false); // this doesn't do much
 
-if(!file_exists(_ENVATO_STATEMENT_CACHE_FILE)){
-    touch(_ENVATO_STATEMENT_CACHE_FILE);
-}
-if(!file_exists(_ENVATO_STATEMENT_CACHE_FILE)){
-    echo "Unable to create "._ENVATO_STATEMENT_CACHE_FILE.". Please ensure correct permissions.";
-    exit;
-}
-if(!file_exists(_VIEWER_CACHE_FILE)){
-    touch(_VIEWER_CACHE_FILE);
-}
-if(!file_exists(_VIEWER_CACHE_FILE)){
-    echo "Unable to create "._VIEWER_CACHE_FILE.". Please ensure correct permissions.";
-    exit;
+foreach(array(_ENVATO_STATEMENT_CACHE_FILE, _ENVATO_API_ITEM_CACHE_FILE, _ENVATO_ITEM_CACHE_FILE, _VIEWER_CACHE_FILE) as $file){
+    if(!file_exists($file)){
+        touch($file);
+    }
+    if(!file_exists($file)){
+        echo "Unable to create ".$file.". Please ensure correct permissions.";
+        exit;
+    }
 }
